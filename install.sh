e190dc4e05953bd7941f0016b1967fbb3eaab619
theBlue0x <3836882+theBlue0x@users.noreply.github.com>
2023-02-20 11:14:28 -0500

    test

--- install.sh
+++ install.sh
@@ -11,14 +11,29 @@ __________.__                _______
      --- Together. We Are the Bank. ---
 '
 
+echo '---------- Updating system ----------------'
+
 apt-get update
+
+echo '---------- System successfully updated! ------------'
+echo '---------- Installing Java -----------------'
 
 apt-get install default-jdk
 
+echo '----------- Java Successfully Installed ----------------'
+echo '------------ Cloning Blue0x Repository ------------'
+
 git clone https://github.com/theBlue0x/node.git
+
+echo '---------Successfully Cloned Blue0x Repo ------------'
 
 cd node
 
+echo '--------- Compiling Blue0x ------------'
+
 ./compile.sh
+
+echo '--------- Successfully Compiled Blue0x ------------'
+echo '--------- Running Blue0x and Syncing Chain ------------'
 
 ./run.sh
