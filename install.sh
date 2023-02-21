8613b883e196ca6f7f4de90300aa332b8331b32b
Blue0x.com <3836882+theBlue0x@users.noreply.github.com>
2023-02-21 10:35:17 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -57,8 +57,8 @@ echo -e "${GREEN_LINE}${aCOLOUR[1]}"
 echo "Please create a new Blue0x account"
 echo "Once you have created your new Blue0x account," 
 echo "please copy/paste your address here"
-echo "Your Blue0x account address: " 
-read add
+echo "Your Blue0x account address: " > /dev/tty
+read add < /dev/tty
 echo -e "$add" > output.txt
 
 echo -e "${GREEN_LINE}${aCOLOUR[1]}"
