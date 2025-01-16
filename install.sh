a7f104512f11afc809bb4ae66bd6c17347238c25
Blue0x <3836882+theBlue0x@users.noreply.github.com>
2025-01-16 10:39:25 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-read -p "Do you want to continue? (y/n): " yn
-
-case $yn in
-    [Yy]* ) echo "Continuing...";;
-    [Nn]* ) echo "Exiting..."; exit;;
-    * ) echo "Invalid response. Please enter y or n.";;
-esac
+echo "Blue0x is currently being updated."
