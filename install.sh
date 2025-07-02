29f9bb05415ee1bb440f8c194a0c2e3d83ca3f32
Blue0x <3836882+theBlue0x@users.noreply.github.com>
2025-07-02 09:56:44 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo ""
 echo " -----------------------------------------------"
 echo " Blue0x is currently being updated.  Stay tuned!"
 echo " -----------------------------------------------"
+echo ""
