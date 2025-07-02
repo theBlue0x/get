e65b4baf1ef7236fff487bc3a4e1a6af0a44dcc3
Blue0x <3836882+theBlue0x@users.noreply.github.com>
2025-07-02 11:58:44 -0400

    Update not_docker.sh

--- not_docker.sh
+++ not_docker.sh
@@ -53,9 +53,16 @@ domain_step
 while true; do
     read -p "You have entered ${domain}.  Is this correct? (y/n) " yn </dev/tty
     case $yn in
-        [Yy]* ) echo "Great! Setting up SSL certificates for your domain..."; break;;
-        [Nn]* ) domain_step;;
-        * ) echo "Please answer yes or no.";;
+        [Yy]* )
+        echo ""
+        echo -e "Great! Setting up SSL certificates for your domain...";
+        echo ""
+        break;;
+        [Nn]* ) 
+        domain_step;;
+        * ) 
+        echo ""
+        echo "Please answer yes or no.";;
     esac
 done
 
@@ -103,7 +110,7 @@ echo -e "${BLUE}Blue0x is now running!"
 echo ""
 echo -e "Please allow a few minutes for the node to start..."
 echo ""
-echo -e "You can view your node here -> ${NC}http://${wanIp}:6876"
+echo -e "You can view your node here -> ${NC}https://${domain}/test"
 echo ""
 echo -e "${BLUE}To stop Blue0x, run ${NC}'sh stop.sh'"
 echo ""
