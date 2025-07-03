e98336dc6828eebe7e32a017ca9cf862ee1206f8
Blue0x <3836882+theBlue0x@users.noreply.github.com>
2025-07-03 10:39:42 -0400

    Create updating.sh

---
+++ updating.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo ""
+echo " -------------------------------------------------"
+echo " Blue0x is currently being updated.  Stay tuned!"
+echo " -------------------------------------------------"
+echo ""
